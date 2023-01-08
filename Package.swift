1b71a2ed9f3abac2f146635015c3f485a0da0509
Jenkins <jenkins@cogosense.com>
2023-01-07 19:37:32 -0800

    Update SPM to version 1.81.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let version = "1.81.0"
 let moduleName = "boost"
-let checksum = "b4c4db2f1c6afebafde5433a69c0028f99476d28e136e44e4ecb23b0c3ca0756"
+let checksum = "da55fd62ad35cac7c730061876309cfa29078e1578928a69cf017d89caa39093"
 
 let package = Package(
     name: moduleName,
