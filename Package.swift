20702879518cc0a5d543754d3a8fd25da109daa5
Jenkins <jenkins@cogosense.com>
2023-01-14 15:37:39 -0800

    Update SPM to version 1.81.2

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let version = "1.81.1"
+let version = "1.81.2"
 let moduleName = "boost"
-let checksum = "1b4dfbb2c92199d7d71109c8237bd7bee8d901f31ba254e870e1049c404d09bf"
+let checksum = "f0cd644ee12f1dd359300830b50442396b3d518e7e3a23a409c9128669003d2d"
 
 let package = Package(
     name: moduleName,
